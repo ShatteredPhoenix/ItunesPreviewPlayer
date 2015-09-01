26478a6bea3617c0ea4e08af408be37d874c1e2a
Humza <hahmed2007@gmail.com>
2015-09-01 13:08:53 +0100

    Initial Commit

---
+++ SwiftApiExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftApiExample
+//
+//  Created by Humza Ahmed on 01/09/2015.
+//  Copyright (c) 2015 Humza Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
